9ed32e4a8f1e98b100154e49c1c29c06dbbd2110
Vangelis Spathonis <baggelisp.keph@gmail.com>
2022-05-15 14:20:20 +0300

    layout module / header comp

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainSectionComponent } from './layout/main-section/main-section.component';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  {
+    path: '',
+    component: MainSectionComponent,
+    children: [
+      {
+        path: '',
+      loadChildren: () => import('./features/search-page/search-page.module').then(m => m.SearchPageModule)
+      }
+    ]
+  }
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -1,12 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header/header.component';
+import { MainSectionComponent } from './main-section/main-section.component';
+import { RouterModule } from '@angular/router';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HeaderComponent,
+    MainSectionComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
+  ],
+  exports: [
+    HeaderComponent,
+    MainSectionComponent,
   ]
 })
 export class LayoutModule { }
